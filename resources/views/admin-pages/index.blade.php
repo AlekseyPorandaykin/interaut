8346a66fbc598b23ffa12ae6004d7f6373d54b2c
Алексей <porandusy@mail.ru>
2017-07-07 01:29:56 +0300

    Добавил favicon

--- resources/views/admin-pages/index.blade.php
+++ resources/views/admin-pages/index.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link id="bs-css" href="/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
